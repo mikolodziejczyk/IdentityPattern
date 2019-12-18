c6620659c39bbd554ff7b1582cec074ed9a8c835
mko <gogs@fake.local>
2019-12-18 12:44:28 +0100

    login page work in progress

---
+++ IdentityPattern/Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using IdentityPattern.Identity;
+using IdentityPattern.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace IdentityPattern.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationUserManager userManager;
+        private readonly ApplicationSignInManager signInManager;
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            this.signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult SignIn(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View(new SignInVM());
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SignIn(SignInVM model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            ApplicationUser user = userManager.FindByName(model.UserName);
+
+            if (user.AccessFailedCount > 3)
+            {
+                // require captcha
+            }
+
+
+            if (!user.EmailConfirmed)
+            {
+                ModelState.AddModelError("", "Your e-mail has not been confirmed. Please find an e-mail in your mailbox and click the link we have sent you to confirm your e-mail.");
+                return View(model);
+            }
+
+            if (!user.IsApproved)
+            {
+                ModelState.AddModelError("", "The user has not been yet approved by the administrator.");
+                return View(model);
+            }
+
+            if (user.IsDisabled)
+            {
+                ModelState.AddModelError("", "The user has been disabled. You cannot log in.");
+                return View(model);
+            }
+
+
+
+            var result = await signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: model.RememberMe, shouldLockout: true);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return RedirectToLocal(returnUrl);
+                //case SignInStatus.LockedOut:
+                //    return View("Lockout");
+                //case SignInStatus.RequiresVerification:
+                //    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                    return View(model);
+            }
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ IdentityPattern/Models/Identity/SignInVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IdentityPattern.Models.Identity
+{
+    public class SignInVM
+    {
+        [Required]
+        [Display(Name = "E-mail")]
+        [EmailAddress]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+}
