1f3e55046a734271e3c82564673a00050d1f8244
mko <gogs@fake.local>
2019-12-20 14:08:38 +0100

    Terms and privacy template

---
+++ IdentityPattern/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityPattern.Controllers
+{
+    public class AboutController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult TermsAndConditions()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
