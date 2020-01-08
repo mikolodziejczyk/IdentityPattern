ccd634b52d0121c122816e521dddaa92073af406
mko <gogs@fake.local>
2020-01-08 13:30:44 +0100

    Sending messages

--- User.Repository/EmailService.cs
+++ User.Repository/EmailService.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using log4net;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -9,9 +11,31 @@ namespace User.Repository
 {
     public class EmailService : IIdentityMessageService
     {
-        public Task SendAsync(IdentityMessage message)
+        public static readonly ILog log = LogManager.GetLogger(typeof(EmailService));
+
+        public virtual Task SendAsync(IdentityMessage message)
         {
             // Plug in your email service here to send an email.
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.To.Add(message.Destination);
+            mailMessage.Subject = message.Subject;
+            mailMessage.Body = message.Body;
+
+            SmtpClient smtpClient = new SmtpClient();
+
+            try
+            {
+                smtpClient.Send(mailMessage);
+            }
+            catch(Exception e)
+            {
+                string logMessage = String.Format("Sending a message to '{0}' with the subject '{1}' failed.", mailMessage.To.ToString(), mailMessage.Subject );
+                log.Error(logMessage, e);
+                throw;
+            }
+
+            log.InfoFormat("A message to '{0}' with the subject '{1}' has been sent.", mailMessage.To.ToString(), mailMessage.Subject);
+            
             return Task.FromResult(0);
         }
 
